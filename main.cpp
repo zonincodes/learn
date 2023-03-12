e9fbb50e0fa40e7f2cfc96bcc525d726d85cf4c0
zonin <business.kariukimartin@gmail.com>
2023-03-12 20:38:52 +0300

    windows display

--- main.cpp
+++ main.cpp
@@ -1,54 +1,75 @@
-#include <iostream>
+#define GLAD_GL_IMPLEMENTATION
+#include <glad.h>
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
 
 
+int main(){
+    int xpos, ypos, height;
+    const char* description;
+    GLFWwindow* windows[4];
 
-void window_size_callback(GLFWwindow *window, int width, int height)
-{
-    glfwSetWindowSize(window, width, height);
-}
-
-// void framebuffer_size_callback(GLFWwindow *window, int width, int height)
-// {
-//     glViewport(0, 0, width, height);
-// }
-
-int main()
-{
-
-    glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
-    GLFWwindow *window;
-    if( !glfwInit() )
-    {
-        fprintf( stderr, "Failed to initialize GLFW\n" );
-        exit( EXIT_FAILURE );
-    }
-    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
-    if (!window)
-    {
-        fprintf( stderr, "Failed to open GLFW window\n" );
-        glfwTerminate();
-        exit( EXIT_FAILURE );
-    }
-       // Main loop
-       glfwSetWindowTitle(window, "My Window");
-
-
-    while( !glfwWindowShouldClose(window) )
-    {
-
-        // Swap buffers
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-        glfwSetWindowSizeCallback(window, window_size_callback);
-        // glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    if(!glfwInit()){
+        glfwGetError(&description);
+        std::cout << "Errors: " << description << std::endl;
+        exit(EXIT_FAILURE);
     }
 
-    // Terminate GLFW
-    glfwTerminate();
+    glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
 
+    glfwGetMonitorWorkarea(glfwGetPrimaryMonitor(), &xpos, &ypos, NULL, &height);
 
+    for(int i = 0; i < 4; i++){
+        const int size = height / 5;
+        const struct {
+            float r, g, b;
+        } colors[] =
+        {
+            {0.95f, 0.32f, 0.11f},
+            {0.50f, 0.80f, 0.16f},
+            {0.f, 0.68f, 0.94f},
+            {0.98f, 0.74f, 0.04f}
+        };
 
+        if(i > 0)
+            glfwWindowHint(GLFW_FOCUS_ON_SHOW, GLFW_FALSE);
 
-    return 0;
+        glfwWindowHint(GLFW_POSITION_X, xpos + size * (1 + (i & 1)));
+        glfwWindowHint(GLFW_POSITION_Y, ypos + size * (1 + (i >> 1)));
+
+        windows[i] = glfwCreateWindow(size, size, "Multi-Window Example", NULL, NULL);
+
+        if(!windows[i]){
+            glfwGetError(&description);
+            std::cout << "Error :" << description << std::endl;
+            glfwTerminate();
+            exit(EXIT_FAILURE);
+        }
+
+        glfwSetInputMode(windows[i], GLFW_STICKY_KEYS, GLFW_TRUE);
+
+        glfwMakeContextCurrent(windows[i]);
+        gladLoadGL();
+        glClearColor(colors[i].r, colors[i].g, colors[i].b, 1.f);
+    }
+
+    for (;;)
+    {
+        for (int i = 0; i < 4; i++){
+            glfwMakeContextCurrent(windows[i]);
+            glClear(GL_COLOR_BUFFER_BIT);
+            glfwSwapBuffers(windows[i]);
+
+            if(glfwWindowShouldClose(windows[i]) || glfwGetKey(windows[i], GLFW_KEY_SPACE))
+            {
+                glfwTerminate();
+                exit(EXIT_SUCCESS);
+            }
+        }
+
+        glfwWaitEvents();
+    }
 }
